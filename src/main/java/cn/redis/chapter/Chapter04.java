835fb4e3d3af545e87c29b21c31c070ef3b5c996
huhuics <hhuics@163.com>
2016-12-27 22:40:07 +0800

    学习redis事务

--- src/main/java/cn/redis/chapter/Chapter04.java
+++ src/main/java/cn/redis/chapter/Chapter04.java
@@ -4,6 +4,13 @@
  */
 package cn.redis.chapter;
 
+import java.util.List;
+import java.util.Set;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+import redis.clients.jedis.Tuple;
+
 /**
  * 第四章学习,主要是redis事务
  * @author HuHui
@@ -15,7 +22,75 @@ public class Chapter04 {
      * @param args
      */
     public static void main(String[] args) {
+        new Chapter04().run();
+    }
 
+    public void run() {
+
+        Jedis conn = new Jedis("168.33.131.55");
+        conn.select(15);
+
+        testListItem(conn);
+    }
+
+    public void testListItem(Jedis conn) {
+
+        System.out.println("初始化操作");
+
+        String seller = "userX";
+        String item = "itemT";
+        conn.sadd("inventory:" + seller, item);
+        Set<String> i = conn.smembers("inventory:" + seller);
+
+        System.out.println("用户的仓库包含以下:");
+        for (String member : i) {
+            System.out.println("  " + member);
+        }
+
+        assert i.size() > 0;
+        System.out.println();
+
+        System.out.println("开始将商品放入市场");
+        boolean l = listItem(conn, item, seller, 12);
+        System.out.println("商品放入市场是否成功?" + l);
+        assert l;
+        Set<Tuple> r = conn.zrangeWithScores("market:", 0, -1);
+
+        System.out.println("市场中存在商品:");
+        for (Tuple tuple : r) {
+            System.out.println("  " + tuple.getElement() + ", " + tuple.getScore());
+        }
+
+        assert r.size() > 0;
+
+    }
+
+    public boolean listItem(Jedis conn, String itemId, String sellerId, double price) {
+
+        String inventory = "inventory:" + sellerId;
+        String item = itemId + '.' + sellerId;
+        long end = System.currentTimeMillis() + 5000;
+
+        while (System.currentTimeMillis() < end) {
+            conn.watch(inventory);
+            if (!conn.sismember(inventory, itemId)) {
+                conn.unwatch();
+                return false;
+            }
+
+            Transaction trans = conn.multi();
+            trans.zadd("market:", price, item);
+            trans.srem(inventory, itemId);
+            List<Object> results = trans.exec();
+
+            //如果返回为空,则说明被监控的key发生了变化
+            if (results == null) {
+                continue;
+            }
+            return true;
+        }
+
+        return false;
     }
 
 }
